247afa24731b4948606bfb85bc3454daa8f2a275
apotasu <apotasu23@gmail.com>
2024-03-07 11:47:29 -0300

    Added code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(interval());
+    }
+
+    public static int interval() {
+        int total = 1;
+        for (int i = 1; i<15; i++){
+            if (soma(i)){
+                total++;
+            }
+        }
+        return total;
+    }
+    public static boolean soma(int n){
+        int soma = 0;
+        for (int i = n; i <= 15; i++){
+            if (i + soma == 15) {
+                return true;
+            } else {
+                soma += i;
+            }
+        }
+        return false;
     }
 }
