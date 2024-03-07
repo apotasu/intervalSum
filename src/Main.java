a471dc02fb85bc616517d260246bf83f0fa6048b
apotasu <apotasu23@gmail.com>
2024-03-07 11:59:46 -0300

    Added new features

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(interval());
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Digite o limite do intervalo");
+        int num = scn.nextInt();
+        System.out.println(interval(num) + " sequências de valores somam exatamente " + num);
     }
 
-    public static int interval() {
+    public static int interval(int num) {
         int total = 1;
-        for (int i = 1; i<15; i++){
-            if (soma(i)){
+        for (int i = 1; i < num; i++) {
+            if (soma(i)) {
                 total++;
             }
         }
         return total;
     }
-    public static boolean soma(int n){
+
+    public static boolean soma(int n) {
         int soma = 0;
-        for (int i = n; i <= 15; i++){
+        for (int i = n; i <= 15; i++) {
             if (i + soma == 15) {
                 return true;
             } else {
